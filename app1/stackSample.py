b9f8f36bf1f4f8e3572d691e31ab2f5f9a115c0c
kenriki <riki.kenmochi@gmail.com>
2022-01-29 15:47:10 +0900

    未使用コードを削除、コード整理

--- app1/stackSample.py
+++ app1/stackSample.py
@@ -1,31 +1,20 @@
 # クラス定義
 class StackA:
     # インスタンス変数
-    max = 100000 # スタック配列の最大サイズ
     top = 0  # スタックの先頭を表すポインタ
-    st = [0] * 100000 
+    st = []
 
     # スタックが空かどうかを判定する
     def isEmpty(self):
-        return (self.top == 0) # スタックサイズが 0 かどうか
-
-    # スタックが満杯かどうかを判定する
-    def isFull(self):
-        return (self.top == max)  # スタックサイズが MAX かどうか
+        return (self.top == 0)  # スタックサイズが 0 かどうか
 
     # push (top を進めて要素を格納)
-    def push(self,v):
-        if self.isFull():
-            print('error: stack is full.')
-            return
+    def push(self, v):
         self.top = self.top + 1
         self.st.append(v)
 
     # pop (top をデクリメントして、top の位置にある要素を返す)
     def pop(self):
-        if self.isFull():
-            print('error: stack is empty.')
-            return -1
         self.top = self.top - 1
         return self.st.pop()
 
@@ -36,6 +25,9 @@ class StackA:
         self.push(3)  # スタックに 3 を積む {1, 2} -> {1, 2, 3}
         self.push(4)  # スタックに 4 を積む {1, 2, 3} -> {1, 2, 3, 4}
         self.push(5)  # スタックに 5 を積む {1, 2, 3, 4} -> {1, 2, 3, 4, 5}
+
+        #for x in self.st:
+        #    print(self.st)
 
         print(self.pop())  # {1, 2, 3, 4, 5} -> {1, 2, 3, 4} で 5 を出力
         print(self.pop())  # {1, 2, 3, 4} -> {1, 2, 3} で 4 を出力
